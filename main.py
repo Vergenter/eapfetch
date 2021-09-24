9cdcbcc955bf7f730034905cd5ebb67295bc12cf
Vergenter <okolok1998@gmail.com>
2021-09-25 00:42:38 +0200

    handle errors

--- main.py
+++ main.py
@@ -16,25 +16,40 @@ ASYNC_TIMEOUT = max(10, INTERVAL.total_seconds()/2)
 
 async def try_get_article():
     async with aiohttp.ClientSession() as session:
-        async with async_timeout.timeout(ASYNC_TIMEOUT):
-            async with session.get(URL) as response:
-                html = await response.text()
-                soup = BeautifulSoup(html, 'html.parser')
-                result = soup.find("article")
-                if isinstance(result, Tag) and 'id' in result.attrs:
-                    logging.debug("try update")
-                    return result["id"] != "post-0"
-                else:
-                    raise NotImplementedError(
-                        "Page without article or without article id")
+        try:
+            async with async_timeout.timeout(ASYNC_TIMEOUT):
+                async with session.get(URL) as response:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, 'html.parser')
+                    result = soup.find("article")
+                    if isinstance(result, Tag) and 'id' in result.attrs:
+                        logging.debug("try update")
+                        return result["id"] != "post-0"
+                    else:
+                        raise NotImplementedError(
+                            "Page without article or without article id")
+        except asyncio.TimeoutError:
+            logging.warning("timeout")
+            return False
 
 
 async def main():
+    next_update = datetime.datetime.now()+INTERVAL
+    error_counter = 3
     while True:
-        next_update = datetime.datetime.now()+INTERVAL
-        if await try_get_article():
+        try:
+            if await try_get_article():
+                break
+        except Exception as ex:
+            logging.error("%s", str(ex))
+            error_counter -= 1
+            if error_counter > 0:
+                continue
+            logging.error("Exit because of errors")
             break
         await asyncio.sleep(max(0, (next_update-datetime.datetime.now()).total_seconds()))
+        next_update += INTERVAL
+        error_counter = 3
     got_article_time = datetime.datetime.now()
     logging.info("Got article!")
     with open("result.txt", 'a+') as out_file:
